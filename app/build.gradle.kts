fc5dc8311a744968cccf83ca23ea4fa73da3593e
xcenia <xcenia94@gmail.com>
2025-02-22 16:32:35 +0700

    add new files

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.12.1"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "io.hexlet"
+
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
+
+repositories { mavenCentral() }
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+    implementation("org.apache.commons:commons-collections4:4.5.0-M3")
+    testImplementation(platform("org.junit:junit-bom:5.12.0-RC2Ю."))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
