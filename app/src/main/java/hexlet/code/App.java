fa5e6f4340ffc368cd975ce897038063d5f8cef6
xcenia <xcenia94@gmail.com>
2025-02-24 11:00:52 +0700

    changes App.java, add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(
+        name = "app",
+        mixinStandardHelpOptions = true,
+        version = "app 1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+
+ class App implements Runnable {
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
+    private boolean helpRequested = false;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+    boolean versionInfoRequested;
+
+    @Override
+    public void run() {
+        System.out.println("Hello, World!");
     }
-}
+
+    public static void main(String[] args) {
+        CommandLine.run(new App(), args);
+    }
+ }
