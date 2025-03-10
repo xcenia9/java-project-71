46160bd590fc5ba05709ec73519bf64737aebeea
xcenia <xcenia94@gmail.com>
2025-03-10 18:04:38 +0700

    changed build.gradle.kts, added yaml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.3")
 }
 
 tasks.test {
