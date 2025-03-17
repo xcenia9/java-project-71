0cb59fdb796be476f4e52c003c975bdc3f4240aa
xcenia <xcenia94@gmail.com>
2025-03-17 10:55:28 +0700

    changed Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,11 +1,14 @@
 package hexlet.code;
 
+import hexlet.code.formatters.Json;
 import hexlet.code.formatters.Plain;
 import hexlet.code.formatters.Stylish;
 
 public class Formatter {
     public static Object getFormatter(String format) {
         switch (format) {
+            case "json":
+                return new Json();
             case "plain":
                 return new Plain();
             case "stylish":
