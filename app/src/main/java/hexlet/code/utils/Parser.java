256fc26927acde3f6baa28cba5cce58a33cac17c
xcenia <xcenia94@gmail.com>
2025-03-17 10:10:08 +0700

    changed Parser.java

--- app/src/main/java/hexlet/code/utils/Parser.java
+++ app/src/main/java/hexlet/code/utils/Parser.java
@@ -3,10 +3,6 @@ package hexlet.code.utils;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Parser {
@@ -18,18 +14,5 @@ public class Parser {
             ObjectMapper yamlObjectMapper = new ObjectMapper(new YAMLFactory());
             return yamlObjectMapper.readValue(content, Map.class);
         }
-    }
-
-    public static Map<String, Object> getData(String filepath) throws Exception {
-        Path fullPath = Paths.get(filepath);
-        if (Files.notExists(fullPath)) {
-            Path currentDirectory = Paths.get(System.getProperty("user.dir"));
-            Path relativePath = currentDirectory.resolve("src/main/resources/files").resolve(filepath);
-            if (Files.exists(relativePath)) {
-                fullPath = relativePath;
-            }
-        }
-        String content = Files.readString(fullPath);
-        return parse(content);
     }
 }
