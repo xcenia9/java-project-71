39444aaab767add8eced3cedd12855c2e6767404
xcenia <xcenia94@gmail.com>
2025-03-17 10:09:17 +0700

    add new test in AppTest.java

--- app/src/test/java/hexlet/code/utils/AppTest.java
+++ app/src/test/java/hexlet/code/utils/AppTest.java
@@ -1,5 +1,7 @@
 package hexlet.code.utils;
 
+import hexlet.code.Differ;
+import hexlet.code.formatters.Stylish;
 import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,14 +26,14 @@ public class AppTest {
 
     @Test
     public void testGetDataJsonFile() throws Exception {
-        Map<String, Object> result = Parser.getData("src/test/resources/test.json");
+        Map<String, Object> result = FileReader.getData("src/test/resources/test.json");
         assertEquals("value1", result.get("key1"));
         assertEquals(2, result.get("key2"));
     }
 
     @Test
     public void testGetDataYamlFile() throws Exception {
-        Map<String, Object> result = Parser.getData("src/test/resources/test.yml");
+        Map<String, Object> result = FileReader.getData("src/test/resources/test.yml");
         assertEquals("value1", result.get("key1"));
         assertEquals(2, result.get("key2"));
     }
@@ -110,5 +112,20 @@ public class AppTest {
         assertEquals(expected, result);
     }
 
+    @Test
+    public void testPlainFormat() {
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("key1", "value1");
+        data1.put("key2", 2);
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("key1", "value2");
+        data2.put("key2", 2);
+        String expected = "Property 'key1' was updated. From value1 to value2\n";
+
+        String result = Differ.generate(data1, data2, "plain");
+
+        assertEquals(expected, result);
+    }
 }
+
 
