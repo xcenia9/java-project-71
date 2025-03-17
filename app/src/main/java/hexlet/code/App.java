7c462ae1eb4a1022b5b6cdca5a16277701bf8977
xcenia <xcenia94@gmail.com>
2025-03-17 08:02:59 +0700

    refactoring App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,7 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import hexlet.code.utils.Differ;
+import hexlet.code.utils.Parser;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -11,10 +9,6 @@ import picocli.CommandLine;
 import picocli.CommandLine.ExitCode;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Spec;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
@@ -25,6 +19,7 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference."
 )
 public class App implements Callable<Integer> {
+
     @Spec
     private CommandSpec spec;
 
@@ -35,7 +30,7 @@ public class App implements Callable<Integer> {
     boolean versionInfoRequested;
 
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish")
-    private String format;
+    private String format = "stylish";
 
     @Parameters(index = "0", arity = "0..1", description = "path to first file")
     private String filepath1;
@@ -45,21 +40,13 @@ public class App implements Callable<Integer> {
 
     @Override
     public Integer call() {
-        if (helpRequested) {
-            spec.commandLine().usage(spec.commandLine().getOut());
-            return ExitCode.OK;
-        }
-        if (versionInfoRequested) {
-            spec.commandLine().printVersionHelp(spec.commandLine().getOut());
-            return ExitCode.OK;
-        }
         if (filepath1 == null || filepath2 == null) {
             System.out.println("Hello, World!");
         } else {
             try {
-                Map<String, Object> data1 = getData(filepath1);
-                Map<String, Object> data2 = getData(filepath2);
-                String diffResult = Differ.generate(data1, data2);
+                Map<String, Object> data1 = Parser.getData(filepath1);
+                Map<String, Object> data2 = Parser.getData(filepath2);
+                String diffResult = Differ.generate(data1, data2, format);
                 System.out.println(diffResult);
             } catch (Exception e) {
                 System.err.println("Error processing files: " + e.getMessage());
@@ -67,29 +54,6 @@ public class App implements Callable<Integer> {
             }
         }
         return ExitCode.OK;
-    }
-
-    public static Map<String, Object> getData(String filepath) throws Exception {
-        Path fullPath = Paths.get(filepath);
-        if (Files.notExists(fullPath)) {
-            Path currentDirectory = Paths.get(System.getProperty("user.dir"));
-            Path relativePath = currentDirectory.resolve("src/main/resources/files").resolve(filepath);
-            if (Files.exists(relativePath)) {
-                fullPath = relativePath;
-            }
-        }
-        String content = new String(Files.readAllBytes(fullPath));
-        return parse(content);
-    }
-
-    public static Map<String, Object> parse(String content) throws Exception {
-        ObjectMapper jsonObjectMapper = new ObjectMapper();
-        try {
-            return jsonObjectMapper.readValue(content, Map.class);
-        } catch (JsonParseException e) {
-            ObjectMapper yamlObjectMapper = new ObjectMapper(new YAMLFactory());
-            return yamlObjectMapper.readValue(content, Map.class);
-        }
     }
 
     public static void main(String[] args) {
