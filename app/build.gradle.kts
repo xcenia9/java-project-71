181289597235d3321c6f21e9a3322bd576b106f8
xcenia <xcenia94@gmail.com>
2025-04-02 10:49:46 +0700

    Update Makefile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     id("checkstyle")
     id("io.freefair.lombok") version "8.12.2"
     id("com.github.ben-manes.versions") version "0.52.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "io.hexlet"
