f365dc2d690080f58f0418a359a11b086b1c230b
xcenia <xcenia94@gmail.com>
2025-04-04 10:26:41 +0700

    Update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 import static hexlet.code.utils.FileReader.getData;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DifferTest {
+public class AppTest {
 
     private String readFile(String path) throws IOException {
         return new String(Files.readAllBytes(Paths.get(path))).trim();
@@ -23,20 +23,20 @@ public class DifferTest {
 
     @BeforeAll
     public static void before() throws Exception {
-        data1 = getData("src/test/resources/introductoryYaml.yml");
-        data2 = getData("src/test/resources/introductoryJson.json");
+        data1 = getData("src/test/resources/files/introductoryYaml.yml");
+        data2 = getData("src/test/resources/files/introductoryJson.json");
     }
 
     @Test
     public void testGenerateStylishFormat() throws IOException {
-        String expectedOutput = readFile("src/test/resources/testStylish.txt");
+        String expectedOutput = readFile("src/test/resources/resultFiles/testStylish.txt");
         String result = Differ.generate(data1, data2, "stylish");
         assertEquals(expectedOutput, result);
     }
 
     @Test
     public void testGenerateJsonFormat() throws IOException {
-        String expectedOutput = readFile("src/test/resources/testJson.json");
+        String expectedOutput = readFile("src/test/resources/resultFiles/testJson.json");
         String result = Differ.generate(data1, data2, "json");
 
         ObjectMapper mapper = new ObjectMapper();
@@ -48,7 +48,7 @@ public class DifferTest {
 
     @Test
     public void testGeneratePlainFormat() throws IOException {
-        String expectedOutput = readFile("src/test/resources/testPlain.txt");
+        String expectedOutput = readFile("src/test/resources/resultFiles/testPlain.txt");
         String result = Differ.generate(data1, data2, "plain");
         assertEquals(expectedOutput, result);
     }
