dc1fc44ae396284ebfd903d3adc581c33d5fbbde
xcenia <xcenia94@gmail.com>
2025-04-14 10:36:29 +0700

    refactoring AppTest.java and Plain.java

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -1,43 +1,47 @@
 package hexlet.code.formatters;
 
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Collection;
 import java.util.stream.Collectors;
-import java.util.Objects;
 
 public class Plain {
     public static String format(Map<String, Object> data1, Map<String, Object> data2) {
         List<String> changes = new ArrayList<>();
 
-        List<String> sortedData1Keys = sortKeys(data1);
-        List<String> sortedData2Keys = sortKeys(data2);
+        List<String> sortedDataKeys = sortKeys(data1, data2);
 
-        for (String key : sortedData1Keys) {
-            if (data2.containsKey(key)) {
+        for (String key : sortedDataKeys) {
+            boolean inData1 = data1.containsKey(key);
+            boolean inData2 = data2.containsKey(key);
+
+            if (inData1 && inData2) {
                 Object value1 = data1.get(key);
                 Object value2 = data2.get(key);
 
                 if (!Objects.equals(value1, value2)) {
-                    changes.add("Property '" + key + "' was updated. From '"
-                            + formatValue(value1) + "' to '" + formatValue(value2) + "'");
+                    changes.add("Property '" + key + "' was updated. From "
+                            + formatValue(value1) + " to " + formatValue(value2));
                 }
-            } else {
+            } else if (inData1 && !inData2) {
                 changes.add("Property '" + key + "' was removed");
-            }
-        }
-        for (String key : sortedData2Keys) {
-            if (!data1.containsKey(key)) {
-                changes.add("Property '" + key + "' was added with value: '" + formatValue(data2.get(key)) + "'");
+            } else if (!inData1 && inData2) {
+                changes.add("Property '" + key + "' was added with value: " + formatValue(data2.get(key)));
             }
         }
 
         return String.join("\n", changes);
     }
 
-    private static List<String> sortKeys(Map<String, Object> data) {
-        return data.keySet().stream()
+    private static List<String> sortKeys(Map<String, Object> data1, Map<String, Object> data2) {
+        Set<String> mergedKeys = new HashSet<>(data1.keySet());
+        mergedKeys.addAll(data2.keySet());
+
+        return mergedKeys.stream()
                 .sorted()
                 .collect(Collectors.toList());
     }
@@ -47,6 +51,8 @@ public class Plain {
             return "null";
         } else if (value instanceof Collection || value instanceof Map) {
             return "[complex value]";
+        } else if (value instanceof String) {
+            return "'" + value + "'";
         } else {
             return String.valueOf(value);
         }
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -58,6 +58,6 @@ public class AppTest {
     public void testGeneratePlainFormat() throws Exception {
         String expected = readFile("src/test/resources/resultFiles/testPlain.txt");
         String result = Differ.generate(firstYaml, secondYaml, "plain");
-        assertEquals(expected.trim().replaceAll("\\s+", " "), result.trim().replaceAll("\\s+", " "));
+        assertEquals(expected.replaceAll("\\s+", "<space>"), result.replaceAll("\\s+", "<space>"));
     }
 }
